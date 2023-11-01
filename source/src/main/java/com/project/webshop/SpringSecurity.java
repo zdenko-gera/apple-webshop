c1f8d92706777beb8a3bd45cafcd5d68933044e1
Varga Zoltán <h270521@stud.u-szeged.hu>
2023-11-01 15:07:43 +0100

    Login valami

---
+++ source/src/main/java/com/project/webshop/SpringSecurity.java
@@ -0,0 +1,63 @@
+package com.project.webshop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .requestMatchers("/SignupPage/**").permitAll()
+                                .requestMatchers("/indexPage").permitAll()
+                                .requestMatchers("/LoginPage").permitAll()
+                                .requestMatchers("*.css").permitAll()
+                                .requestMatchers("*.svg").permitAll()
+                                .requestMatchers("*.jpeg").permitAll()
+                                .requestMatchers("*.png").permitAll()
+                                .requestMatchers("*.jpg").permitAll()
+                                .requestMatchers("/users").hasRole("ADMIN")
+                ).formLogin(
+                        form -> form
+                                .loginPage("/LoginPage")
+                                .loginProcessingUrl("/loginUser")
+                                .defaultSuccessUrl("/indexPage", true)
+                                .permitAll()
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/login")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+}
