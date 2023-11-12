1db309055d0f0e39b8a5b4842cd1fe093d3d8178
Gera Zdenkó <h157872@stud.u-szeged.hu>
2023-11-12 22:23:08 +0100

    Rendelés törlése funkció (félkész)

--- source/src/main/java/com/project/webshop/DAO/OrderDAO.java
+++ source/src/main/java/com/project/webshop/DAO/OrderDAO.java
@@ -2,11 +2,16 @@ package com.project.webshop.DAO;
 
 import com.project.webshop.AppConfig;
 import com.project.webshop.Models.CartModel;
+import com.project.webshop.Models.OrderModel;
+import com.project.webshop.Models.ProductModel;
 import com.project.webshop.Models.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -58,11 +63,31 @@ public class OrderDAO {
 
     /**
      * Töröl egy meglévő rendelést, ha rövid időn belül törli
-     * @param email A felhasználó email címe
+     //* @param email A felhasználó email címe
      * @param orderID A rendelés azonosítója
      * @return
      */
-    public boolean deleteOrder(String email, int orderID) {
-        return false;
+    public boolean deleteOrder(int orderID) {
+        String sqlCode ="DELETE FROM orders WHERE orders.orderID ="+orderID+";";
+        return jdbcTemplate.update(sqlCode) == 1;
+    }
+
+    /**
+     * Visszaadja egy rendelés OrderModeljét ID alapján.
+     * @param id A visszatérítendő rendelés ID-je
+     * @return A kért rendelés OrderModellje
+     */
+    public OrderModel getOrderById(int id) {
+        String sqlCode = "SELECT * FROM orders WHERE order.orderID = ?";
+        Map<String, Object> order;
+        try {
+            order = jdbcTemplate.queryForMap(sqlCode, id);
+        } catch (EmptyResultDataAccessException erdae) {
+            System.err.println(erdae.toString());
+            return null;
+        }
+
+        return new OrderModel((int) order.get("orderID"), (int) order.get("price"),
+                order.get("email").toString(), (LocalDateTime) order.get("orderDate"));
     }
 }
