af071e164845a0aa554234d8e32aad293a14df30
vencel <vencel>
2023-11-13 22:57:03 +0100

    productpage css javítások

--- source/src/main/java/com/project/webshop/DAO/ProductDAO.java
+++ source/src/main/java/com/project/webshop/DAO/ProductDAO.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Table(name="product")
